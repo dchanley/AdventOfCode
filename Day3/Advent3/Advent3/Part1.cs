175360a602d945470941d6662b320d0c27e86172
David Hanley <dchanley@asu.edu>
2023-12-04 08:00:37 -0700

    started day 3

---
+++ Day3/Advent3/Advent3/Part1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent3
+{
+    internal class Part1
+    {
+        private int solution;
+        public Part1()
+        {
+            solution = 0;
+            List<ElfDataObject> list = new List<ElfDataObject>();
+            StreamReader sr = new StreamReader("ElfData.txt");
+            while (!sr.EndOfStream)
+            {
+                ElfDataObject obj = new ElfDataObject();
+                obj.Chars = sr.ReadLine().ToCharArray();
+                list.Add(obj);                
+            }
+            sr.Close();
+            int rowCounter = 0;
+            
+            foreach (ElfDataObject obj in list)
+            {
+                int collumnCounter = 0;
+                foreach (char c in obj.Chars)
+                {
+                    if (c != '.' && !System.Char.IsDigit(c))
+                    {
+
+                    }
+                    collumnCounter++;
+                }
+                rowCounter++;
+            }
+        }
+    }
+}
---
+++ Day3/Advent3/Advent3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1 part1 = new Part1();
+        }
+    }
+}
