41a3b7ec324c70418e47420bdfe2f490a04ecddd
dchanley <dhanley@veritasglobal.com>
2023-12-06 14:35:27 -0700

    finished day 3

--- Day3/Advent3/Advent3/Part2.cs
+++ Day3/Advent3/Advent3/Part2.cs
@@ -1,30 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Advent3
 {
-    internal class Part1
+    class Part2
     {
         private List<string> solutionHolder = new List<string>();
+        private List<string> gearHolder = new List<string>();
         private int solution;
-        public Part1()
+        public Part2()
         {
             solution = 0;
             List<ElfDataObject> list = new List<ElfDataObject>();
-            StreamReader sr = new StreamReader("ElfData.txt");
+            System.IO.StreamReader sr = new System.IO.StreamReader("ElfData.txt");
             while (!sr.EndOfStream)
             {
                 ElfDataObject obj = new ElfDataObject();
                 obj.Chars = sr.ReadLine().ToCharArray();
-                list.Add(obj);                
+                list.Add(obj);
             }
             sr.Close();
             int rowCounter = 0;
-            
+
             foreach (ElfDataObject obj in list)
             {
                 int collumnCounter = 0;
@@ -32,43 +32,59 @@ namespace Advent3
                 {
                     if (c != '.' && !System.Char.IsDigit(c))
                     {
+                        gearHolder = new List<string>();
                         if (rowCounter > 0)
                         {
-                            getSolution(list[rowCounter-1],collumnCounter);
+                            getSolution(list[rowCounter - 1], collumnCounter,c);
                         }
-                        getSolution(list[rowCounter], collumnCounter);
+                        getSolution(list[rowCounter], collumnCounter, c);
                         if (rowCounter < list.Count)
                         {
-                            getSolution(list[rowCounter + 1], collumnCounter);
+                            getSolution(list[rowCounter + 1], collumnCounter, c);
+                        }
+                        if (gearHolder.Count > 1)
+                        {
+                            int tempNumber = 1;
+                            foreach (string temp in gearHolder)
+                            {
+                                tempNumber = tempNumber * Convert.ToInt32(temp);
+                            }
+                            solutionHolder.Add(tempNumber.ToString());
                         }
                     }
                     collumnCounter++;
                 }
                 rowCounter++;
             }
+            foreach (string tempNumber in solutionHolder)
+            {
+                Console.WriteLine(tempNumber);
+                solution = solution + Convert.ToInt32(tempNumber);
+            }
             Console.WriteLine(solution);
             Console.ReadLine();
         }
-        public void getSolution(ElfDataObject tester, int originalIndex)
-        {
+        public void getSolution(ElfDataObject tester, int originalIndex, char symbol)
+        {            
             int indexTester = originalIndex - 1;
-            while (indexTester <= (originalIndex +1))
+            while (indexTester <= (originalIndex + 1))
             {
                 if (indexTester >= 0)
                 {
                     if (System.Char.IsDigit(tester.Chars[indexTester]))
                     {
-                        getData(tester.Chars,indexTester);
+                        getData(tester.Chars, indexTester, symbol);
                     }
-                }                
+                }
                 indexTester++;
-            }
+            }            
         }
-        public void getData(Char[] array, int index)
+        public void getData(Char[] array, int index, char symbol)
         {
-            string tempNumber ="";
+            string tempNumber = "";
             int tempIndex = index;
-            while (tempIndex >= 0) { 
+            while (tempIndex >= 0)
+            {
                 if (tempIndex >= 0 && System.Char.IsDigit(array[tempIndex]))
                 {
                     tempNumber = array[tempIndex] + tempNumber;
@@ -85,7 +101,7 @@ namespace Advent3
             {
                 if (tempIndex >= 0 && System.Char.IsDigit(array[tempIndex]))
                 {
-                    tempNumber = tempNumber + array[tempIndex] ;
+                    tempNumber = tempNumber + array[tempIndex];
                     array[tempIndex] = '.';
                 }
                 else
@@ -93,10 +109,15 @@ namespace Advent3
                     break;
                 }
                 tempIndex++;
+            }            
+            if (symbol == '*')
+            {
+                gearHolder.Add(tempNumber);
             }
-            Console.WriteLine(tempNumber);
-            solutionHolder.Add(tempNumber);
-            solution = solution + Convert.ToInt32(tempNumber);
+            else {
+
+            }
+            
 
         }
     }
--- Day5/Advent5/Advent5/Program.cs
+++ Day5/Advent5/Advent5/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Advent3
+namespace Advent5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Part1 part1 = new Part1();
-            Part2 part2 = new Part2();
         }
     }
 }
